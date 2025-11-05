d716a20bbcd075c4d26748797cdb756ab1055f46
mahmadayubnayab <mahmadayubnayab@gmail.com>
2025-11-05 15:42:08 +0430

    Update EvenOrOddNumber.java

--- EvenOrOddNumber.java
+++ EvenOrOddNumber.java
@@ -1,15 +1,18 @@
-package evenoroddnumber;
 import java.util.Scanner;
+
 public class EvenOrOddNumber {
-   public static void main(String [] args){
-     Scanner input = new Scanner(System.in);   
-       
-    /*  System.out.println("enter a number ");
-       double number = input.nextDouble(); 
-       if (number %2 == 0){
-           System.out.println("the number  is even");
-       }else if(number %2 == 1){
-           System.out.println("the number is odd");
-       }*/
-   } 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("enter a number");
+        int number = input.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("the number is even");
+        } else {
+            System.out.println("the number is odd");
+        }
+
+        input.close();
+    }
 }
