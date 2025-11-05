df8e69b424369e2f0316ff977c9425bb421f35d7
mahmadayubnayab <mahmadayubnayab@gmail.com>
2025-11-05 15:41:23 +0430

    Update CelsiusToFarnahaite.java

--- CelsiusToFarnahaite.java
+++ CelsiusToFarnahaite.java
@@ -1,18 +1,15 @@
-package celsius;
 import java.util.Scanner;
 
 public class CelsiusToFarnahaite {
-  public static void main(String [] args){
-  Scanner input = new Scanner(System.in);
-      
-   /*   System.out.println("enter any number of celsius");
-      double celsius = input.nextDouble();
-      double farnahite = (((9* celsius)/5)+32);
-      System.out.println("this is the degree of " + celsius + "celsius which you entered is equal to" + "degree of farnahaite."); */
-      
-      
-      
-      
-         
-  }  
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("enter any number of celsius");
+        double celsius = input.nextDouble();
+        double farnahaite = (9 * celsius / 5) + 32;
+
+        System.out.println(celsius + " Celsius is equal to " + farnahaite + " Fahrenheit");
+
+        input.close();
+    }
 }
