48457a9308ed8f37beca203adc2d8dcf1b1474ab
mahmadayubnayab <mahmadayubnayab@gmail.com>
2025-11-05 15:42:53 +0430

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,30 +1,24 @@
-package firstprogtame;
 import java.util.Scanner;
-public class Main{
-public static void main(String [] args){
-   Scanner input = new Scanner(System.in);
-         
-      System.out.println("enter your phone number");
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("enter your phone number");
         String number = input.nextLine();
-        if(number.startsWith("070")){
-       System.out.println("your number is afghan bisim");
-            
-        }else if(number.startsWith("079")){
+
+        if (number.startsWith("070")) {
+            System.out.println("your number is afghan bisim");
+        } else if (number.startsWith("079")) {
             System.out.println("your number is roshan");
-        
-        }else if(number.startsWith("078")){
+        } else if (number.startsWith("078")) {
             System.out.println("your number is etisalat");
-        
-        }else if(number.startsWith("074")){
+        } else if (number.startsWith("074")) {
             System.out.println("your number is salam");
-        }else{
+        } else {
             System.out.println("your number is wrong");
         }
 
-   
-      
-     }
-    
-     
-     
- }
+        input.close();
+    }
+}
