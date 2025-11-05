e7df4e1bab26bf58f3befe884deeb75b2fee4920
mahmadayubnayab <mahmadayubnayab@gmail.com>
2025-11-05 15:39:44 +0430

    Update Area.java

--- Area.java
+++ Area.java
@@ -1,11 +1,15 @@
-package area;
+import java.util.Scanner;
+
 public class Area {
-  public static void main(String [] args) {
-  /*    double radius = 2;
-       double area = 3.14 * radius * radius;
-        System.out.println("the area of this circle is "  + area);*/
-      
-      
-      
-  } 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circle: ");
+        double radius = input.nextDouble();
+
+        double area = Math.PI * radius * radius; // Using Math.PI for better accuracy
+        System.out.println("The area of the circle is: " + area);
+
+        input.close();
+    }
 }
