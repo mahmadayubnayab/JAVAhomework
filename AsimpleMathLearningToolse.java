6b2d101fcd2337ab9c6da00a493ca2af031cac14
mahmadayubnayab <mahmadayubnayab@gmail.com>
2025-11-05 15:40:30 +0430

    Update AsimpleMathLearningToolse.java

--- AsimpleMathLearningToolse.java
+++ AsimpleMathLearningToolse.java
@@ -1,22 +1,22 @@
-package mathsimplehomework;
 import java.util.Scanner;
+
 public class AsimpleMathLearningToolse {
- public static void main(String[] args){
-      Scanner input = new Scanner(System.in);
-     
-   /*  int num1 = (int) (Math.random() *10);
-     int num2 = (int) (Math.random() *10);
-        
-     System.out.println("what is " +num1 + " + "+ num2 +" += ?");  
-   int userresult = input.nextInt();   
-    int programresult = num1 + num2;     
-        
-        
-        if (userresult ==programresult){
-       System.out.println("you are very good");   
-            
-        }else {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int num1 = (int) (Math.random() * 10);
+        int num2 = (int) (Math.random() * 10);
+
+        System.out.println("what is " + num1 + " + " + num2 + " = ?");
+        int userresult = input.nextInt();
+        int programresult = num1 + num2;
+
+        if (userresult == programresult) {
+            System.out.println("you are very good");
+        } else {
             System.out.println("you are not very good");
-        }*/
- }   
+        }
+
+        input.close();
+    }
 }
