fc7756c60fa96a0237a4ef0ad0543d424de0cd97
mahmadayubnayab <mahmadayubnayab@gmail.com>
2025-11-05 15:40:53 +0430

    Update Bmi.java

--- Bmi.java
+++ Bmi.java
@@ -1,24 +1,27 @@
-package bmi;
-  import java.util.Scanner;
+import java.util.Scanner;
+
 public class Bmi {
-  public static void main(String [] args ){
-    Scanner input = new Scanner(System.in);  
-      
-       /*     System.out.println("enter your weight");  
-   
-      float weight = input.nextFloat();
-          System.out.println("enter your height");
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("enter your weight");
+        float weight = input.nextFloat();
+
+        System.out.println("enter your height");
         float height = input.nextFloat();
-        float Bmi = weight / (height * height);
-        if(Bmi < 18.5 ){
-            System.out.println("you are weight");
-        }else if (Bmi >= 18.5 && Bmi <25){
+
+        float bmi = weight / (height * height);
+
+        if (bmi < 18.5) {
+            System.out.println("you are underweight");
+        } else if (bmi >= 18.5 && bmi < 25) {
             System.out.println("your body is normal");
-    
-        }else if (Bmi >= 25 && Bmi < 30){
-            System.out.println("you are a lot weight");
-        }else if (Bmi >30){
-            System.out.println("you are fool weight");
-  }  */
-  }  
+        } else if (bmi >= 25 && bmi < 30) {
+            System.out.println("you are overweight");
+        } else if (bmi >= 30) {
+            System.out.println("you are obese");
+        }
+
+        input.close();
+    }
 }
