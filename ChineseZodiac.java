c9cc76d4afce06611e271b187123945c73c32b20
mahmadayubnayab <mahmadayubnayab@gmail.com>
2025-11-05 15:41:49 +0430

    Update ChineseZodiac.java

--- ChineseZodiac.java
+++ ChineseZodiac.java
@@ -1,49 +1,40 @@
-package zodiac;
 import java.util.Scanner;
+
 public class ChineseZodiac {
- public static void main(String [] args){
-    Scanner input = new Scanner(System.in); 
-     
-  /*   System.out.println("enter a year pleas");
-    int year = input.nextInt(); 
-        if (year % 12 == 0) {
-        System.out.println("this is monkey year");   
-        } else if(year % 12 == 1){
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("enter a year please");
+        int year = input.nextInt();
+
+        int zodiac = year % 12;
+
+        if (zodiac == 0) {
+            System.out.println("this is monkey year");
+        } else if (zodiac == 1) {
             System.out.println("this is rooster year");
-        
-        
-        } else if(year % 12 == 2){
+        } else if (zodiac == 2) {
             System.out.println("this is dog year");
-        
-        } else if(year % 12 == 3){
+        } else if (zodiac == 3) {
             System.out.println("this is pig year");
-        
-        } else if(year % 12 == 4){
-            System.out.println("this is Rat year");
-        
-        } else if(year % 12 == 5){
+        } else if (zodiac == 4) {
+            System.out.println("this is rat year");
+        } else if (zodiac == 5) {
             System.out.println("this is ox year");
-        
-        } else if(year % 12 == 6){
+        } else if (zodiac == 6) {
             System.out.println("this is tiger year");
-        } else if(year % 12 == 7){
+        } else if (zodiac == 7) {
             System.out.println("this is rabbit year");
-        
-        } else if(year % 12 == 8){
-            System.out.println("this is dragone year");
-        
-        
-         } else if(year % 12 == 9){
-            System.out.println("this is snak year");
-        
-        
-        } else if(year % 12 == 10){
+        } else if (zodiac == 8) {
+            System.out.println("this is dragon year");
+        } else if (zodiac == 9) {
+            System.out.println("this is snake year");
+        } else if (zodiac == 10) {
             System.out.println("this is horse year");
-        
-        
-        
-        } else if(year % 12 == 11){
+        } else if (zodiac == 11) {
             System.out.println("this is sheep year");
- } */
- }  
+        }
+
+        input.close();
+    }
 }
