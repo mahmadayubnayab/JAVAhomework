ad474561470ee724ed0157dda290f0ef79ed360d
mahmadayubnayab <mahmadayubnayab@gmail.com>
2025-11-10 22:12:10 +0430

    Add files via upload

---
+++ Exercise2solution.java
@@ -0,0 +1,143 @@
+
+package exercise2solution;
+import java.util.Scanner;
+
+public class Exercise2solution {
+
+
+    
+    public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+        
+       //question number 2.1
+       
+      /*  System.out.println("enter a degree in celsiuse ");
+    double celsiuse  = input.nextDouble();
+    double farnahit = (9/5)*( celsiuse + 32);
+        System.out.println("the" + celsiuse + "degree of celsiuse  = " + farnahit + "f" );*/
+    
+    
+    
+    
+     
+     // question number 2.2
+     /*   System.out.println("enter the radius and length of cylinder ");
+    double radius  = input.nextDouble();
+    double length  = input.nextDouble();
+
+    double area = radius * radius * 3.14;
+    double volume = area * length;
+        System.out.println("this is area of cylinder = " + area );
+         System.out.println("this is volume  of cylinder = " + volume );*/
+     
+     
+   // question number 2.3 
+      /*  System.out.println("enter a value for foot" );
+     int foot = input.nextInt();
+     
+     double  onefoot = 0.305;
+     double meter = foot * onefoot;
+        System.out.println(foot + " foot  = " + meter + " m ");*/
+      
+      // question number 2.4 
+     /*   System.out.println("enter a value for pound  ");
+    int pound = input.nextInt();
+    
+    double onepound = 0.454;
+    double kilogram = pound * onepound;
+        System.out.println(pound + "ppound =  " + kilogram + "kg");*/
+     
+   
+     // question number 2.7 
+      /*  System.out.println("enter minutes ");
+     double minutes = input.nextDouble();
+     
+     double day = minutes / 1440;
+     double year = day/360;
+        System.out.println( minutes + " minutes = " + day + "day" );
+        System.out.println( minutes + " minutes = " + year + "year " );*/
+      
+     // question number 2.9
+      /*   System.out.println("enter the v0 , v1 and t ");
+      double v0 = input.nextDouble();  
+      double v1 = input.nextDouble();
+      double t  = input.nextDouble();
+
+      double taghjeel = (v1-v0)/ t ;
+        System.out.println("tagheel = " + taghjeel + "m2");*/
+
+     
+     // question number 2.10
+      /*  System.out.println("enter amount of water ");
+     double amount = input.nextDouble();
+      
+     System.out.println("enter initial tempreture of water ");
+     double initialtempreture = input.nextDouble();
+     
+     System.out.println("enter final tempreture of water ");
+     double finaltemreture = input.nextDouble();
+    
+     double needenergy = amount * (finaltemreture - initialtempreture) * 4184;
+     
+        System.out.println("the energy needed =" + needenergy); */ 
+      
+      
+     
+     
+   // question number 2.11  
+     
+  /*      System.out.print("enter the number of year ");
+        double number = input.nextDouble();
+        int worldfirstpopulution = 40;
+        System.out.println("the world first populution was = " + worldpopulution +" m"); 
+        double nowpopulution = number * 40;
+        System.out.print("now  the world populution is =  " + nowpopulution + "m");*/
+        
+      
+  // question number 2.12
+  /* System.out.print("enter the speed  of plane  v = ");   
+  double speed = input.nextDouble();
+  
+  
+          System.out.print("enter the taghjel   of plane  a = ");   
+  double taghjel = input.nextDouble();
+  double v  = speed ; 
+  double a = taghjel;
+  
+  double length = (v *v )/ 2*a;
+            System.out.println("this is minimum way  length  for plane " + length );*/
+        
+ 
+       
+     
+       
+     // questin number 2.13  
+     /*   System.out.println("enter the value ");
+       double value = input.nextDouble();
+       double  b = value ;
+      b=11.56;
+        System.out.println("11 dallars ");
+        System.out.println("56 cent");*/
+     
+     
+     
+     // question number 2.23;
+   /*     System.out.println("enter the distance of driving ");
+    double distance = input.nextDouble();
+        System.out.println("enter miles per gallon ");
+    double miles = input.nextDouble();
+   
+          System.out.println("enter price  per gallon ");
+        double price = input.nextDouble();
+
+    double costdriving  = miles * price;
+        System.out.println("the cost o driving = " + costdriving);*/  
+   
+
+    
+    
+    
+     
+    }
+    
+}
