56d8017366dc3801251474f2cb1b19f50062436e
mahmadayubnayab <mahmadayubnayab@gmail.com>
2025-11-10 22:10:13 +0430

    Add files via upload

---
+++ Exercise1solution.java
@@ -0,0 +1,150 @@
+
+
+package exercise1solution;
+
+import java.util.Scanner;
+
+public class Exercise1solution {
+
+    
+    public static void main(String[] args) {
+   Scanner input = new Scanner(System.in);
+   
+   // Exercise one solution;
+   
+   // question number 1;
+   // area of circle ;
+ /*  double  radius = 4;
+   double areacircle = radius * radius * 3.14;
+        System.out.println("this is area of circle "  + areacircle  +"m2"  );*/
+ 
+ 
+ // question number 2;
+ // perimeter of rectangle;
+/* int length = 5;
+ int width = 3;
+ int perimeter = (2 * length) + (2 * width);
+        System.out.println("the perimeter of rectangle is = " + perimeter + "m" );*/
+ 
+ 
+// question number 3;
+// swap two number; 
+  /* int a  = 5;
+  int b = 6;
+  a = a+b;
+  b = a-b;
+  a = a-b;
+        System.out.println("the a = 5 " + "now  a = " + a  );
+        System.out.println("the b = 6 " + "now b = " + b );*/
+ 
+    
+    // question number 4;
+    // simple interest;
+  
+  /*  int principle = 2000;
+    int interestonemonth  = 150;
+    int oneyear = 12;
+    
+    double totalprinciple = principle + (interestonemonth * oneyear );
+       
+    System.out.println("your one month interest = " + interestonemonth);
+        System.out.println( "first your principle was " +  principle  +   " afg"    +      "after one year your principle =  "  + totalprinciple + " afg "); */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // question number 5;
+    // even or add number;
+    
+        /* System.out.println("enter one number ");
+        int number = input.nextInt();
+        int a = number 
+;
+        if (a%2==0 ){
+            System.out.println("this number is add = " + a );
+        }else{
+            System.out.println("this number is even = " + a);
+        }  */
+    
+    
+    // question number 6;
+    //  sum of first natural number ;
+     
+   /* int n = 1;
+        System.out.println("enter one  n natural number ");
+       int number  = input.nextInt();
+       int a = number ;
+       int sum = a * (a + 1)/2;
+        System.out.println("this is sum of first" +    number    +" natural numbers = " +  sum  );*/
+   
+   
+   
+   // question number 7;
+   // conver celsius to farnahit;
+   
+      /*  System.out.println("enter the degree of celsius ");
+    int celsius = input.nextInt();
+    int c = celsius;
+    double farnahit = (9 / 5 * c) + 32;
+        System.out.println("the celsius " + celsius + " degree = " + farnahit + "f"); */
+      
+      
+      // question number 8;
+      // maximum of two number ;
+      
+     /*   System.out.println("enter two numbers ");
+      int Fnumber = input.nextInt();
+       int Snumber = input.nextInt();
+
+      int a = Fnumber;
+      int b = Snumber;
+      
+      if(a>b){
+          System.out.println("the first number is larger than second number " + a );
+      }else {
+                System.out.println("the second  number is larger than first  number " + b);
+      }  */
+      
+      
+       // question number 9;
+       // check lea year;
+       
+     /*   System.out.println("enter the  day of year ");
+      int day = input.nextInt();
+      
+      int a = day;
+      
+      if (a <= 365){
+        System.out.println("this year is not leap year " + a);
+    } else {
+          System.out.println("this year is leap year " + a );  
+      }  */
+     
+     
+     
+     // question number 10;
+     // reverse a number ;
+     
+     /*   System.out.println("enter one number ");
+      int number = input.nextInt();
+     int a = number;
+      int reversed = 0;
+      while(number != 0){
+      int digit = number % 10;
+      reversed = reversed * 10 + digit;
+      number /= 10;
+      
+      }
+        System.out.println("the  " + a + " reverse  = " + reversed); */
+      
+      
+  
+    }
+    
+}
