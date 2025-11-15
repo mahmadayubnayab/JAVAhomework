168886ea36b03495f94c6310bb9b9b57bb569b6e
mahmadayubnayab <mahmadayubnayab@gmail.com>
2025-11-15 22:16:40 +0430

    Add files via upload

---
+++ PointOfRectangle.java
@@ -0,0 +1,31 @@
+
+package pointofrectangle;
+import java.util.Scanner;
+
+
+public class PointOfRectangle {
+
+   
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+   
+
+ /*  System.out.print("enter a point with two coordinates :  ");
+       double  x = input.nextInt();
+       double  y = input.nextInt();
+       double high = 5;
+       double width = 10;
+       
+       if(x<width/2  && y<high/2 ){
+           System.out.println("this " + ( x+ " " +  y) + "point is in the rectangle  ");
+       }else {
+           System.out.println("this " + (x + " "  +  y ) + "point is not in the rectangle  ");
+       }*/
+     
+
+
+
+    }
+    
+}
