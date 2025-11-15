c599cea5e860072f5b74767e34fc071679be22d9
mahmadayubnayab <mahmadayubnayab@gmail.com>
2025-11-15 22:20:17 +0430

    Add files via upload

---
+++ WeigthCost.java
@@ -0,0 +1,40 @@
+
+package weigthcost;
+import java.util.Scanner;
+public class WeigthCost {
+
+   
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+        System.out.println("enter the weight of package ");
+      double weight = input.nextDouble();
+       
+    //    0: 0<weight<=1 cost= 3.5$;
+    //    0: 1<weight<=3 cost= 5.5$ ;
+    //    0: 3<weight<=10 cost= 8.5$;
+    //    0: 10<weight<=20 cost= 10.5$;
+    
+ /*  double  w = weight;
+    if(w<0){
+        System.out.println("package does not have cost");
+    }else if(w<1){
+        System.out.println("the package cost is = 3.5$");
+    }else if(w<3){
+            System.out.println("the package cost is = 5.5$");
+
+    }else if(w<10){
+                System.out.println("the package cost is = 8.5$");
+
+    }else if(w<20){
+                System.out.println("the package cost is = 10.5$");
+
+    }else {
+        System.out.println("the package cost is indeffinit");
+    }*/
+
+              
+              
+  }
+    
+}
