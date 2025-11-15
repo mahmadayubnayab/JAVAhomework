091168cbec752086382dc5b00a9a7af8e9315f55
mahmadayubnayab <mahmadayubnayab@gmail.com>
2025-11-15 22:11:55 +0430

    Add files via upload

---
+++ DayOfWeeks.java
@@ -0,0 +1,62 @@
+
+package dayofweeks;
+import java.util.Scanner;
+
+
+public class DayOfWeeks {
+
+    
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+     /*   System.out.println("enter year  =  ");
+      int year = input.nextInt();
+        System.out.println("enter month 1-12 = ");     
+      int month = input.nextInt();
+        System.out.println("enter the day of the month  1-31= ");
+       int dayofmonth = input.nextInt();
+       
+      int q = dayofmonth;
+      int m = month;
+      int j = year / 100;
+      int k = year % 100;
+         // h = day of the week
+       
+    int h = (q  + ((26 *(m+1))/10) + k + k/4 + j/4 + 5 * j)%7; 
+   if(h==0){
+           System.out.println(" this day is  of the week = satarday " ); 
+
+   }else if (h==1){
+              System.out.println(" this day is  of the week = sunday " ); 
+
+   }else if(h==2){
+              System.out.println(" this day is  of the week = monday " ); 
+
+   }else if(h==3){
+                  System.out.println(" this day is  of the week = tusday " ); 
+
+       
+   }else if(h==4){
+              System.out.println(" this day is  of the week = wensday " ); 
+
+   }else if(h==5){
+              System.out.println(" this day is  of the week = tharsday " ); 
+
+   }else {
+              System.out.println(" this day is  of the week = friday " ); 
+
+   }*/
+
+
+   
+
+
+
+
+
+   
+    
+    
+    }
+}
+
