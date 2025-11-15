cb2e15174a165a9a765edfcf5ff362772ff474b4
mahmadayubnayab <mahmadayubnayab@gmail.com>
2025-11-15 22:14:08 +0430

    Add files via upload

---
+++ OperatorUsing.java
@@ -0,0 +1,23 @@
+
+package operatorusing;
+import java.util.Scanner;
+
+
+public class OperatorUsing {
+
+    
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+      /*  System.out.println("enter one integer number ");
+     int number =input.nextInt();
+
+     
+        System.out.println("is" + number  +   "divisible 5 and 6 ?" + (number %5==0 && number %6==0));
+        System.out.println("is" + number  +   "divisible 5 or 6 ?" + (number %5==0 || number %6==0));
+        System.out.println("is" + number  +    "divisible 5 or 6 ,but not both ?" + (number %5==0));*/
+
+     
+    }
+    
+}
