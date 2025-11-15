efec1ea94b95cc91d9b8d688559bbffe27aa23e7
mahmadayubnayab <mahmadayubnayab@gmail.com>
2025-11-15 22:19:38 +0430

    Add files via upload

---
+++ TRinglePerimeter.java
@@ -0,0 +1,31 @@
+
+package tringleperimeter;
+import java.util.Scanner;
+
+  
+
+public class TRinglePerimeter {
+
+    
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+     /*   System.out.println("enter the three edges of traingle ");
+       int a = input.nextInt();
+        int b = input.nextInt();
+       int c  = input.nextInt();
+     
+        System.out.println("this is the first edges of traingle  = " + a );
+         System.out.println("this is the second  edges of triangle  = " + b );
+        System.out.println("this is the third edges of traingle  = " + c );
+      
+        if(a>b && a>c){
+           int perimeter = a + b + c;
+            System.out.println("the traingle a edge is iarge  = " + a +    "and perimeter of traingle = " + perimeter);
+        }else {
+            System.out.println("condition is false and a edge is smal =  " + a );
+        }*/
+    
+    }
+    
+}
