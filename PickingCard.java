99512409ac297792d09a05aa1bb701d0dfb9e0a2
mahmadayubnayab <mahmadayubnayab@gmail.com>
2025-11-15 22:14:51 +0430

    Add files via upload

---
+++ PickingCard.java
@@ -0,0 +1,24 @@
+
+package pickingcard;
+import java.util.Scanner;
+
+
+public class PickingCard {
+
+    
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+  /*  String [] suits = {"clubs" , "dimonds" , "hearts" , "spades"};
+     String [] ranks = {"ace" , "2" ,"3","4","5","6","7","8","9","10","jack","queen","king"};
+     
+     int x = (int)(Math.random()*4);
+     int y = (int)(Math.random()*13);
+
+        System.out.println("the card you picked is " + ranks[y]+   " of " +   suits[x]);*/
+  
+  
+  
+    }
+    
+}
