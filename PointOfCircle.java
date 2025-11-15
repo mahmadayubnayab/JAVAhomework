4aaf3b260e47bb5d0cf02151e078eeecb86a035a
mahmadayubnayab <mahmadayubnayab@gmail.com>
2025-11-15 22:17:26 +0430

    Add files via upload

---
+++ PointOfCircle.java
@@ -0,0 +1,30 @@
+
+package pointofcircle;
+import java.util.Scanner;
+
+
+public class PointOfCircle {
+
+    
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+
+ /*   System.out.print("enter one point x2 and y2 ");
+       int x2 = input.nextInt();
+       int y2 = input.nextInt();
+      int x1 = 0;
+      int y1 = 0;
+       double radius = 10;
+       double distance = Math.sqrt(x2-x1)*(x2-x1) + (y2-y1)*(y2-y1);
+       if(distance<=10){
+           System.out.println("this poin is inside at circle ");
+       }else {
+           System.out.println("this point is outside of circle ");
+       }*/
+     
+
+
+    }
+    
+}
