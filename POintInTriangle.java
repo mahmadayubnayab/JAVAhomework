b8bcb125e559b3d6c7a54d151b7f089a6e528e0e
mahmadayubnayab <mahmadayubnayab@gmail.com>
2025-11-15 22:15:34 +0430

    Add files via upload

---
+++ POintInTriangle.java
@@ -0,0 +1,80 @@
+
+package pointintriangle;
+import java.util.Scanner;
+
+
+public class POintInTriangle {
+
+    
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+     /*   System.out.print("enter a point x and y coordinates : ");
+    int x = input.nextInt();
+    int y = input.nextInt();
+     
+    if(x>=0 && y>=0){
+        System.out.println("this point is in the traingle ");
+    }else{
+        System.out.println("this point is not in the traingle "); 
+   }*/
+    
+    
+    
+    
+    
+    // the two rectangle show;
+    
+    /*    System.out.print("enter first rectangle  center x ,y coordinates width and height: ");
+    int x    = input.nextInt();
+     int y   = input.nextInt();
+    int r1width = input.nextInt();
+    int r1height = input.nextInt();
+
+    
+     System.out.print("enter second rectangle center x ,y coordinates width and height: ");
+    int a    = input.nextInt();
+     int b   = input.nextInt();
+    int r2width = input.nextInt();
+    int r2height = input.nextInt();
+
+   if(r1width>r2width && r1height>r2height){
+       System.out.println("second rectangle is in of first rectangle  ");
+   } else if (r1width<r2width &&  r1height<r2height){
+       System.out.println("first rectangle is in of second rectangle ");
+   }else{
+       System.out.println("both rectangle is equal to ");
+   }*/
+    
+    
+    
+    // the two circles  shows;
+    
+     /*   System.out.print("enter the circle1 center x , y coordinates and radius ");
+         double x = input.nextInt();
+         double  y = input.nextInt();
+         double  c1radius = input.nextInt();
+
+         
+          System.out.print("enter the circle2 center x , y coordinates and radius ");
+         double  a = input.nextInt();
+         double  b = input.nextInt();
+         double  c2radius = input.nextInt();
+    
+         double distancetwocenters = Math.sqrt((x-a)*(x-a)+(y-b)*(y-b));
+         double D = distancetwocenters;
+         System.out.println("this is distance between centers = " + D);
+         
+         
+         if(D <= c1radius - c2radius ){
+             System.out.println("circle2 is inside circle1");
+         }else if( D >= c1radius + c2radius ){
+             System.out.println("circle2 overlaps circle1");
+         }else {
+             System.out.println("both circles is shining");
+         }*/
+    
+    
+    }
+    
+}
